0c6caeae2b811ee55eabd57462c13887b57472e2
박동휘 <daniel1502@naver.com>
2023-09-08 15:29:45 +0900

    Design : Create signup UI

--- frontend/src/templates/repassword-template/RepasswordTemplate.js
+++ frontend/src/templates/repassword-template/RepasswordTemplate.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RepasswordTemplate.css';
 
 const RepasswordTemplate = () => {
-  return <div className="repassword">비밀번호 재설정 페이지입니다.</div>;
+  const [state, setState] = useState({ password: '', repassword: '' });
+
+  return (
+    <div className="repassword">
+      <p className="repassword-title">비밀번호 재설정</p>
+      <div className="repassword-input">
+        <input
+          className="input"
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          variant="outlined"
+          value={state.password}
+          onChange={(e) => setState({ ...state, password: e.target.value })}
+        />
+        <span className="border"></span>
+      </div>
+      <div className="repassword-input">
+        <input
+          className="input"
+          type="password"
+          placeholder="비밀번호를 다시 한번 입력해주세요"
+          variant="outlined"
+          value={state.repassword}
+          onChange={(e) => setState({ ...state, repassword: e.target.value })}
+        />
+        <span className="border"></span>
+      </div>
+      <br />
+      <div className="repassword-input">
+        <button className="repassword-button">재설정하기</button>
+      </div>
+    </div>
+  );
 };
 
 export default RepasswordTemplate;
