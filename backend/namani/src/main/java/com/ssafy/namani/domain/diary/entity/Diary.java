f367c5eaa71ed5c7dc7daa06a34b4084e28770d6
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-09-08 16:07:14 +0900

    #S09P22A410-132 [S09P22A410-132] [BE-feat/132-diary-entity] feat Feat: Diary Entity 구현 Resolves: #132

---
+++ backend/namani/src/main/java/com/ssafy/namani/domain/diary/entity/Diary.java
@@ -0,0 +1,41 @@
+package com.ssafy.namani.domain.diary.entity;
+
+import com.ssafy.namani.domain.member.entity.Member;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Diary {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id; // 일기 아이디
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member; // 사용자
+
+    @NotNull
+    private String content; // 일기 내용
+
+    @NotNull
+    private String comment; // 일기 코멘트
+
+    @NotNull
+    private Integer stampType; // 일기 도장 종류
+
+    @NotNull
+    private Date regDate; // 일기 등록 날짜
+}
