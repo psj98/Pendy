4f8bd1955a635d4fa82e3a6c4fcf4bdd8c09ca99
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-09-08 16:45:41 +0900

    #S09P22A410-135 [S09P22A410-135] [BE-feat/135-total-goal-entity] feat Feat: TotalGoal Entity 구현 Resolves: #135

---
+++ backend/namani/src/main/java/com/ssafy/namani/domain/goal/entity/TotalGoal.java
@@ -0,0 +1,37 @@
+package com.ssafy.namani.domain.goal.entity;
+
+import com.ssafy.namani.domain.member.entity.Member;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TotalGoal {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id; // 목표 아이디
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member; // 사용자
+
+    @NotNull
+    private Integer goalAmount; // 목표 소비 금액
+
+    @NotNull
+    private Date goalDate; // 목표 설정 날짜
+
+    @NotNull
+    private String aiAnalysis; // AI 분석
+}
