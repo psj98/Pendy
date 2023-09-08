fd81f8175d2e1ef5ba2710dcb0b81c4aacba3656
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-09-08 16:50:08 +0900

    #S09P22A410-136 [S09P22A410-136] [BE-feat/136-goal-by-category-entity] feat Feat: GoalByCategory Entity 구현 Resolves: #136

---
+++ backend/namani/src/main/java/com/ssafy/namani/domain/goal/entity/GoalByCategory.java
@@ -0,0 +1,34 @@
+package com.ssafy.namani.domain.goal.entity;
+
+import com.ssafy.namani.domain.category.entity.Category;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoalByCategory {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id; // 카테고리 별 목표 아이디
+
+    @ManyToOne
+    @JoinColumn(name = "total_goal_id")
+    private TotalGoal totalGoal; // 전체 목표
+
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category; // 카테고리
+
+    @NotNull
+    private Integer categoryGoalAmount; // 목표 소비 금액
+}
