ef178e22791161c6daab8637efd44dbabeaf6e9f
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-09-11 14:54:21 +0900

    Fix: Emotion 엔티티 수정

--- backend/namani/src/main/java/com/ssafy/namani/domain/emotion/entity/Emotion.java
+++ backend/namani/src/main/java/com/ssafy/namani/domain/emotion/entity/Emotion.java
@@ -2,13 +2,17 @@ package com.ssafy.namani.domain.emotion.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 
 @Entity
 @Getter
 public class Emotion {
-	@Id
-	private Integer emotionScore; // 감정 점수
-	private String emotionName; // 감정명
+
+    @Id
+    private Integer emotionScore; // 감정 점수
+
+    @NotNull
+    private String emotionName; // 감정명
 }
