ca2efcdc512e52d19bcd6cacd476d55b834ad8af
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-09-12 17:58:50 +0900

    Refactor: BaseException 추가

--- backend/namani/src/main/java/com/ssafy/namani/domain/member/MemberService.java
+++ backend/namani/src/main/java/com/ssafy/namani/domain/member/MemberService.java
@@ -3,9 +3,11 @@ package com.ssafy.namani.domain.member;
 import java.util.UUID;
 
 import com.ssafy.namani.domain.member.dto.MemberRegisterRequestDto;
+import com.ssafy.namani.global.response.BaseException;
 
 public interface MemberService {
-	void register(MemberRegisterRequestDto memberRegisterRequestDto);
 
-	void connectAccountWithMember(String accountNumber, UUID memberId);
+    void register(MemberRegisterRequestDto memberRegisterRequestDto) throws BaseException;
+
+    void connectAccountWithMember(String accountNumber, UUID memberId) throws BaseException;
 }
