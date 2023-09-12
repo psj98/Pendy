a2b413cdbab89a59bec10bc1e8bcbd2d39d2d819
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-09-12 17:58:28 +0900

    Refactor: @Repository 추가

--- backend/namani/src/main/java/com/ssafy/namani/domain/member/MemberRepository.java
+++ backend/namani/src/main/java/com/ssafy/namani/domain/member/MemberRepository.java
@@ -2,9 +2,11 @@ package com.ssafy.namani.domain.member;
 
 import com.ssafy.namani.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
     Member findByEmail(String email);
 }
