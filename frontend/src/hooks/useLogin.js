e1f4543e3ba166d72c7e98c861678e8ca1d7bb37
박동휘 <daniel1502@naver.com>
2023-09-19 16:02:14 +0900

    Comment : hook 로그 추가

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function useLogin() {
+  console.log('useLogin');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const location = useLocation();
 
