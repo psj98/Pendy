2bfb59f848924889538b362956dd4683040e3480
김동현 <hyeon.dh.kim@gmail.com>
2023-09-19 09:55:18 +0900

    refactor: prompt 영문 지시문 추가

--- ML/diaries/Mkdiary.py
+++ ML/diaries/Mkdiary.py
@@ -36,25 +36,38 @@ def mkdiary(req):
     limit_amount = req_limit_amount.iloc[0,0] #목표 금액
     # limit_amount = req_limit_amount["금액"][0]
 
-        # 각 소비내역 시간 분류대로 텍스트화
-    #컬럼 뽑아서
-    #하나씩 붙이기
-    consume_list = []
-    feeling = ["매우 불만족","불만족","보통","만족","매우 만족"]
+    # feeling = ["매우 불만족","불만족","보통","만족","매우 만족"]
+    feeling = ["Shit", "Dissatisfied", "Neutral", "Satisfied", "Stoked"]
+
+    consume_list = ""
     for i in req_consume_list.columns:
-        consume_list += str(i)+":"
-        for j in req_consume_list[i]:
-            # 3이 보통, feeling 리스트 참조
-            consume_list += feeling[j]+","
-    consume_list += " "
-    consume_txt = ''.join(a for a in consume_list)
+        consume_one = str(i) + ":" + str(req_consume_list[i][0]) + "Won " + str(
+            feeling[(req_consume_list[i][1])]) + "\n"
+        consume_list += consume_one
 
 
-
-    #소비 내역으로 input_txt 생성
+    #소비 내역으로 instructions 생성
     #고정적인 입력값
-    # 프롬프트 수정 필요(영어로 role등 명시해서(기능은 잘 돌아감, 토큰이 많이듬))
-    input_head_txt =(
+    # 영어 지시문 혹은 한국어 지시문 둘 다 작성해두었습니다
+    instructions = """
+        Act as an 5-year-old child
+        [Instructions]
+        Create a diary entry that is compact and easily digestible in one read.
+        Adhere to the response format below for crafting the diary entry and providing feedback.
+        [Steps]
+    
+        Title: Craft an engaging title summarizing the content in about 10 characters.
+        Body: Write the main content of the diary.
+        Feedback: From the perspective of an elementary school teacher, give economic feedback considering the budget, using informal language in about 20 characters.
+        [Response Format]
+        {
+        "Title": "engaging and concise title",
+        "Body": "main content of the diary",
+        "Feedback": "economic feedback considering the budget as an elementary school teacher."
+        }
+    """
+
+    instructions_kor ="""
        "아래의 정보들로 일기를 써주세요\n"
         "일기 작성자는 5살입니다\n"
         "한번에 확인할 수 있게 컴팩트하게 작성해주세요\n"
@@ -65,12 +78,12 @@ def mkdiary(req):
 
         "위 양식을 지켜주세요\n"
         "피드백은 초등학교 선생님의 입장에서 반말로 목표금액을 고려하여 경제적인 관점에서 작성해주세요\n")
-
-    input_limit_txt = "\n소비한도금액:"+limit_amount+"\n"
-
+    """
+    # input_limit_txt = "\n소비한도금액:"+limit_amount+"\n"
+    input_limit_txt = "SpendingLimit:" + limit_amount + "\n"
 
     # 최종적으로 GPT에 입력할 텍스트
-    input_txt = input_head_txt+consume_txt+input_limit_txt
+    input_txt = instructions+consume_list+input_limit_txt
     result = llm(input_txt)
 
     ret["message"] = result
--- ML/feedback/Mkreport.py
+++ ML/feedback/Mkreport.py
@@ -28,24 +28,45 @@ def mkreport(req):
     #     "fashion": [12000, 10000],
     #     "culture": [0, 10000]
     # }
+
     #소비 내역으로 input_txt 생성
     #고정적인 입력값
-    # 프롬프트 수정 필요(영어로 role등 명시해서(기능은 잘 돌아감, 토큰이 많이듬))
-    input_head_txt = (
-        "입력은 아래의 형식과 같습니다\n"
-        "카테고리명 : 사용 금액 , 제한 금액\n"
-        "각 영단어는 매핑된 오른쪽 한글에 해당됩니다\n"
+    #한국어, 영어 버전 둘 다 작성
+    instructions_kor = """
+        입력은 아래의 형식과 같습니다
+        카테고리명 : 사용 금액 , 제한 금액
+        각 영단어는 매핑된 오른쪽 한글에 해당됩니다
 
-        "food : 식비\n"
-        "traffic : 교통\n"
-        "online : 온라인 쇼핑\n"
-        "offline : 오프라인 쇼핑\n"
-        "cafe : 카페/간식\n"
-        "housing : 주거/통신\n"
-        "fashion : 패션/미용\n"
-        "culture : 문화/여가\n"
-        "주어진 카테고리별 사용금액과 제한금액을 보고 공백 포함 150자 내외로 피드백을 한글로 컴팩트하게 작성해주세요\n"
-    )
+        food : 식비
+        traffic : 교통
+        online : 온라인 쇼핑
+        offline : 오프라인 쇼핑
+        cafe : 카페/간식
+        housing : 주거/통신
+        fashion : 패션/미용
+        culture : 문화/여가
+        주어진 카테고리별 사용금액과 제한금액을 보고 공백 포함 150자 내외로 피드백을 한글로 컴팩트하게 작성해주세요
+    """
+    instructions = """
+        Act as a Financial Analyst
+
+        [Instructions]
+        "The input should be in the following format:
+        'Category Name: Expense amount, Limit amount'
+        Each English word corresponds to the mapped Korean term on the right."
+
+        food : 식비
+        traffic : 교통
+        online : 온라인 쇼핑
+        offline : 오프라인 쇼핑
+        cafe : 카페/간식
+        housing : 주거/통신
+        fashion : 패션/미용
+        culture : 문화/여가
+
+        Based on the given expense and limit amounts for each category, please write compact feedback in Korean, within approximately 150 characters including spaces.
+        }
+    """
 
     # 중간 txt
     # req_cols = req.keys()
@@ -58,7 +79,7 @@ def mkreport(req):
     consume_txt = ''.join(a for a in consume_list)
 
     # 최종적으로 GPT에 입력할 텍스트
-    input_txt = input_head_txt+consume_txt
+    input_txt = instructions + consume_txt
     result = llm(input_txt)
 
     ret["message"] = result
