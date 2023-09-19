e0ad813bde68fd790527ec71a812d47970429085
박동휘 <daniel1502@naver.com>
2023-09-19 15:06:08 +0900

    Feat : 계좌 로그인 기능

---
+++ frontend/src/utils/handleBankLogin.js
@@ -0,0 +1,17 @@
+//계좌 로그인
+import axiosCreate from '../axiosCreate';
+
+const handleBankLogin = (accountNumber, bankCode, accountPassword) => {
+  console.log('handleBankLogin');
+  const data = {
+    accountNumber: accountNumber,
+    bankCode: bankCode,
+    accountPassword: accountPassword,
+  };
+
+  const serverUrl = '/api/accounts/login';
+
+  return axiosCreate.post(serverUrl, data);
+};
+
+export default handleBankLogin;
