c5b3364497f38e0aa40816360b99da088cf6b345
27kanghan <27kanghan@naver.com>
2023-09-20 16:24:27 +0900

    Feat: 월별 목표 조회

--- frontend/src/templates/goal-template/GoalTemplate.js
+++ frontend/src/templates/goal-template/GoalTemplate.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './GoalTemplate.css';
 import DonutChart from '../../components/common/donut-chart/DonutChart';
 import BarChart from '../../components/common/bar-chart/BarChart';
-
+import handleGoalDetail from '../../utils/handleGoalDetail';
+import format from 'date-fns/format';
 const GoalTemplate = () => {
+  useEffect(() => {
+    //age, salary
+    const age = sessionStorage.getItem('age');
+    const salary = sessionStorage.getItem('salary');
+    const curDate = format(Date.now(), "yyyy-MM-dd'T'HH:mm:ss.SSS'+09:00'");
+
+    const fetchData = async () => {
+      try {
+        const response = await handleGoalDetail(age, salary, curDate);
+
+        console.log('goal', response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  });
+
   return (
     <div className="goal-template">
       <div className="goal-container">
