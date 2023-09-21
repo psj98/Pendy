558766cd9c6ff0a3d71d6c1503ebd2a03dba0a5e
ynwoo <ynwookim98@gmail.com>
2023-09-21 17:36:21 +0900

    fix: 목표바 타입 지정 가능

--- frontend/src/components/common/goal-bar/GoalBar.js
+++ frontend/src/components/common/goal-bar/GoalBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './GoalBar.css';
 
-const GoalBar = ({ color, current, goal }) => {
+const GoalBar = ({ color, current, goal, type }) => {
   const midpoint = (current / goal) * 100 + '%';
+  const barType = ['rectangle'].includes(type) ? type : 'default';
   return (
     <div
-      className="goal-bar"
+      className={[`goal-bar-${barType}`].join(' ')}
       style={{ '--midpoint': midpoint, '--color': color }}
     >
       <p>{Math.round((current / goal) * 100)}%</p>
