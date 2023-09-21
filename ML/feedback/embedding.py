6639b4fc5d57371d28a6c99ecde28a5ace35ceaf
김동현 <hyeon.dh.kim@gmail.com>
2023-09-21 10:46:19 +0900

    init: 카드상품 embedding

--- ML/feedback/embedding.py
+++ ML/feedback/embedding.py
@@ -1,35 +1,33 @@
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.docstore.document import Document
 
 from ML.openaikey import apikey
+import os
 
-db_path = "../vector_chromadb"
-file_name = "../product.txt"
-file=open(file_name,"r",encoding="utf-8")
-# file.write()
-# file.close()
+if __name__=="__main__":
 
-plain_txt = file.read()
+    db_path = "./vector_faissdb"
+    file_name = "./product.txt"
 
-doc_chunks = []
+    os.environ["OPENAI_API_KEY"] = apikey
 
-for line in plain_txt:
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000, # 최대 청크 길이
-        separators=["\n\n", "\n", "\t",".", "!", "?", ",", " ", ""], #  텍스트를 청크로 분할하는 데 사용되는 문자 목록
-        chunk_overlap=0, # 인접한 청크 간에 중복되는 문자 수
-    )
-    chunks = text_splitter.split_text(line)
-    for i, chunk in enumerate(chunks):
-        doc = Document(
-            page_content=chunk, metadata={"page": i, "source": file_name}
-        )
-        doc_chunks.append(doc)
-		#persist_directory 을 명시하지 않으면 현재 디렉토리에 저장합니다
-persist_directory = "./vector_chromadb"
+    loader = TextLoader(file_name,encoding='utf-8')
+    documents = loader.load()
+    #chunk_size : 최대 청크 길이, chunk_overlap : 인접한 청크 간에 중복되는 문자 수
+    # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0) # 빠름
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0) # 느림
+    docs = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings(api_key = apikey)
-db = FAISS.from_documents(doc_chunks, embeddings)
-db.save_local(db_path)
+    embeddings = OpenAIEmbeddings()
+    db = FAISS.from_documents(docs, embeddings)
+    db.save_local(db_path)
+
+    # 경로가 문제인가 확인하기 위한 코드-> 경로문제는 아님
+    # f = open(file_name, 'r',encoding='utf-8')
+    # plain_txt = f.read()
+    # f.close()
+    # print(plain_txt)
