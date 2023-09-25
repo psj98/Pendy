37e35b9166361c94d7fba37eb7bb58c8d98c1838
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-09-25 15:11:22 +0900

    Feat: 회원 가입 페이지 CSS 적용

--- frontend/src/components/signup/account-modal/AccountModal.js
+++ frontend/src/components/signup/account-modal/AccountModal.js
@@ -7,7 +7,7 @@ import { InputLabel, MenuItem } from '@mui/material';
 import handleSendCode from '../../../utils/handleSendCode';
 import handleAuthentication from '../../../utils/handleAuthentication';
 
-const AccountModal = ({ index, closeModal, handleAccountChange }) => {
+const AccountModal = ({ index, closeModal, handleAccountChange, accounts }) => {
   const [accountNumber, setAccountNumber] = useState('');
   const [bankName, setBankName] = useState('');
   const [bankCode, setBankCode] = useState('');
@@ -52,12 +52,20 @@ const AccountModal = ({ index, closeModal, handleAccountChange }) => {
   const onSendCodeClick = async (event) => {
     event.preventDefault();
     try {
+      if (accounts.includes(accountNumber)) {
+        alert('이미 등록한 계좌입니다.');
+        return;
+      }
+
       const response = await handleSendCode(bankCode, accountNumber);
       if (response.data.code === 1000) {
         console.log('SendCode success');
         alert('계좌로 전송된 예금주명 뒤 숫자 3자리를 확인해주세요');
         setShowVerificationInput(true);
         startTimer();
+      } else if (response.data.code === 3002) {
+        console.error(response.data.code + ' ' + response.data.message);
+        alert('계좌 번호가 존재하지 않습니다.');
       } else {
         console.error(response.data.code + ' ' + response.data.message);
         alert('인증에 실패했습니다.');
@@ -113,84 +121,93 @@ const AccountModal = ({ index, closeModal, handleAccountChange }) => {
 
   return (
     <div className="modal">
-      <div className="modal-content">
-        <div className="close-button-container">
-          <button onClick={closeModal}>x</button>
-        </div>
-        <h2>계좌 인증</h2>
-        <div className="input-container">
-          <h3>은행 선택</h3>
-          <FormControl fullWidth size="small">
-            <InputLabel id="select-label">은행 선택</InputLabel>
-            <Select
-              labelId="select-label"
-              id="simple-select"
-              value={bankName}
-              label="은행 선택"
-              onChange={(e) => onBankNameChange(e)}
-              fullWidth={true}
-            >
-              <MenuItem value="KDB산업은행">KDB산업은행</MenuItem>
-              <MenuItem value="IBK기업은행">IBK기업은행</MenuItem>
-              <MenuItem value="KB국민은행">KB국민은행</MenuItem>
-              <MenuItem value="NH농협은행">NH농협은행</MenuItem>
-              <MenuItem value="우리은행">우리은행</MenuItem>
-              <MenuItem value="씨티은행">씨티은행</MenuItem>
-              <MenuItem value="DGB대구은행">DGB대구은행</MenuItem>
-              <MenuItem value="부산은행">부산은행</MenuItem>
-              <MenuItem value="광주은행">광주은행</MenuItem>
-              <MenuItem value="제주은행">제주은행</MenuItem>
-              <MenuItem value="전북은행">전북은행</MenuItem>
-              <MenuItem value="경남은행">경남은행</MenuItem>
-              <MenuItem value="새마을금고">새마을금고</MenuItem>
-              <MenuItem value="신협">신협</MenuItem>
-              <MenuItem value="우체국예금보험">우체국예금보험</MenuItem>
-              <MenuItem value="하나은행">하나은행</MenuItem>
-              <MenuItem value="신한은행">신한은행</MenuItem>
-              <MenuItem value="케이뱅크">케이뱅크</MenuItem>
-              <MenuItem value="카카오뱅크">카카오뱅크</MenuItem>
-              <MenuItem value="토스뱅크">토스뱅크</MenuItem>
-            </Select>
-          </FormControl>
-          <h3>계좌 번호</h3>
+      <div className="account-modal-content">
+        <button className="account-close-button" onClick={closeModal}>
+          X
+        </button>
+        <h2 className="account-modal-title">계좌 인증</h2>
+        <div className="account-input-container">
+          <div className="account-bank-select-form">
+            <FormControl fullWidth size="small">
+              <InputLabel id="select-label">은행 선택</InputLabel>
+              <Select
+                labelId="select-label"
+                id="simple-select"
+                value={bankName}
+                label="은행 선택"
+                onChange={(e) => onBankNameChange(e)}
+                fullWidth={true}
+              >
+                <MenuItem value="KDB산업은행">KDB산업은행</MenuItem>
+                <MenuItem value="IBK기업은행">IBK기업은행</MenuItem>
+                <MenuItem value="KB국민은행">KB국민은행</MenuItem>
+                <MenuItem value="NH농협은행">NH농협은행</MenuItem>
+                <MenuItem value="우리은행">우리은행</MenuItem>
+                <MenuItem value="씨티은행">씨티은행</MenuItem>
+                <MenuItem value="DGB대구은행">DGB대구은행</MenuItem>
+                <MenuItem value="부산은행">부산은행</MenuItem>
+                <MenuItem value="광주은행">광주은행</MenuItem>
+                <MenuItem value="제주은행">제주은행</MenuItem>
+                <MenuItem value="전북은행">전북은행</MenuItem>
+                <MenuItem value="경남은행">경남은행</MenuItem>
+                <MenuItem value="새마을금고">새마을금고</MenuItem>
+                <MenuItem value="신협">신협</MenuItem>
+                <MenuItem value="우체국예금보험">우체국예금보험</MenuItem>
+                <MenuItem value="하나은행">하나은행</MenuItem>
+                <MenuItem value="신한은행">신한은행</MenuItem>
+                <MenuItem value="케이뱅크">케이뱅크</MenuItem>
+                <MenuItem value="카카오뱅크">카카오뱅크</MenuItem>
+                <MenuItem value="토스뱅크">토스뱅크</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
           <div className="modal-input">
             <div className="modal-input-part1">
               <input
                 type="number"
                 name="accountNumber"
+                className="modal-account-input"
                 placeholder="-를 제외하고 입력"
                 value={accountNumber}
                 onChange={(e) => setAccountNumber(e.target.value)}
               />
             </div>
             <div className="modal-input-part2">
-              <button onClick={onSendCodeClick}>인증 요청</button>
+              <button className="send-code-btn" onClick={onSendCodeClick}>
+                인증 요청
+              </button>
             </div>
           </div>
         </div>
+
         {showVerificationInput && (
-          <div className="input-container">
-            <h3>인증번호 입력</h3>
-            <div className="modal-input">
-              <div className="modal-input-part1">
+          <>
+            <div className="modal-code-container">
+              <div className="modal-code-input">
                 <input
                   type="number"
                   name="authCode"
+                  className="modal-code-input-box"
                   placeholder="인증번호를 입력하세요"
                   value={authCode}
                   onChange={(e) => setAuthCode(e.target.value)}
                 />
+                <div className="timer-text">{timer}</div>
               </div>
-              <div className="modal-input-part2">
-                <div className="modal-input-part2">
-                  <button onClick={onAuthenticationClick}>인증</button>
-                </div>
+              <div className="modal-code-btn-box">
+                <button
+                  className="modal-code-btn"
+                  onClick={onAuthenticationClick}
+                >
+                  인증
+                </button>
               </div>
             </div>
-            <div className="timer-text">{timer}초</div>
-          </div>
+          </>
         )}
-        <button onClick={onSaveClick}>등록</button>
+        <button className="account-regist-btn" onClick={onSaveClick}>
+          등록
+        </button>
       </div>
     </div>
   );
