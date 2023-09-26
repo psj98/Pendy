b953398680ac7be7086972a41a92d8bff68e4a11
27kanghan <27kanghan@naver.com>
2023-09-26 15:57:16 +0900

    Refactor: 헤더 메뉴 변경

--- frontend/src/components/common/header/Header.js
+++ frontend/src/components/common/header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
-import MenuListUser from '../menu-list-user/MenuListUser';
-import MenuListGuest from '../menu-list-guest/MenuListGuset';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
-// 페이지 헤더
 const Header = ({ isLoggedIn }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
+  const onClickLogout = () => {
+    console.log('logout');
+    localStorage.removeItem('accessToken');
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('name');
+    sessionStorage.removeItem('age');
+    sessionStorage.removeItem('salary');
+    sessionStorage.removeItem('accountList');
+    navigate('/', { replace: true });
+  };
+
   return (
     <div className="header">
       <div className="header-logo-container">
@@ -14,7 +29,58 @@ const Header = ({ isLoggedIn }) => {
         </Link>
       </div>
       <div className="header-menu-container">
-        {isLoggedIn ? <MenuListUser /> : <MenuListGuest />}
+        <div className="menu-list">
+          {isLoggedIn ? (
+            <div className="sub-menu">
+              <div className="sub-menu-container">
+                <Link
+                  to={'/setting'}
+                  className={`link-content ${
+                    isActive('/setting') ? 'active-link' : ''
+                  }`}
+                >
+                  개인정보
+                </Link>
+              </div>
+              <div className="sub-menu-container">
+                <Link
+                  to={'/goal'}
+                  className={`link-content ${
+                    isActive('/goal') ? 'active-link' : ''
+                  }`}
+                >
+                  목표설정
+                </Link>
+              </div>
+              <div className="sub-menu-container" onClick={onClickLogout}>
+                로그아웃
+              </div>
+            </div>
+          ) : (
+            <div className="sub-menu">
+              <div className="sub-menu-container">
+                <Link
+                  to={'/login'}
+                  className={`link-content ${
+                    isActive('/login') ? 'active-link' : ''
+                  }`}
+                >
+                  로그인
+                </Link>
+              </div>
+              <div className="sub-menu-container">
+                <Link
+                  to={'/login/signup'}
+                  className={`link-content ${
+                    isActive('/login/signup') ? 'active-link' : ''
+                  }`}
+                >
+                  회원가입
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
