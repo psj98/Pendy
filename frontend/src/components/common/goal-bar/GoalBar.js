019ed4f53c03d73c078628585b6e6071d7d2bd07
김동현 <hyeon.dh.kim@gmail.com>
2023-09-27 15:48:53 +0900

    fix : 월간분석 NaN 해결

--- frontend/src/components/common/goal-bar/GoalBar.js
+++ frontend/src/components/common/goal-bar/GoalBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './GoalBar.css';
 
-const GoalBar = ({ color, current, goal, type, textcolor }) => {
-  const midpoint = (current / goal) * 100 + '%';
-  const barType = type != null ? type : 'default';
-  const textColor = textcolor === 'red' ? 'red' : 'black';
+const GoalBar = ({ color, current=0, goal=1, type, textcolor}) => {
+    // NAN 문제 해결
+    if (goal === 0){
+        goal = 1;
+    }
+    const midpoint = (current / goal) * 100 + '%';
+    const barType = type != null ? type : 'default';
+    const textColor = textcolor === 'red' ? 'red' : 'black';
 
   return (
     <div
