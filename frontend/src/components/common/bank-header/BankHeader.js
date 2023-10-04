7414c6e074b8c7f3f29dffad3a043da2a28f9a2f
박동휘 <daniel1502@naver.com>
2023-10-04 12:53:37 +0900

    Refactor : BankHeader 불필요한 선언 개선

--- frontend/src/components/common/bank-header/BankHeader.js
+++ frontend/src/components/common/bank-header/BankHeader.js
@@ -1,27 +1,12 @@
+// 계좌 페이지용 헤더
 import React from 'react';
 import './BankHeader.css';
 import { Link, useNavigate } from 'react-router-dom';
 
-// 페이지 헤더
 const BankHeader = ({ isBankLoggedIn }) => {
   const navigate = useNavigate();
 
-  const renderLoginOrLogout = () => {
-    if (isBankLoggedIn) {
-      return (
-        <div onClick={onLogout} className="bank-header-link-content">
-          로그아웃
-        </div>
-      );
-    } else {
-      return (
-        <Link to={'/bank/login'} className="bank-header-link-content">
-          로그인
-        </Link>
-      );
-    }
-  };
-
+  //로그아웃 클릭 시 동작
   const onLogout = () => {
     console.log('bankLogout');
     sessionStorage.removeItem('accountNumber');
@@ -37,8 +22,18 @@ const BankHeader = ({ isBankLoggedIn }) => {
           PENDY BANK
         </Link>
       </div>
-      <div className="bank-header-blank"></div>
-      <div className="bank-header-menu-container">{renderLoginOrLogout()}</div>
+      <div className="bank-header-blank" />
+      <div className="bank-header-menu-container">
+        {isBankLoggedIn ? (
+          <div onClick={onLogout} className="bank-header-link-content">
+            로그아웃
+          </div>
+        ) : (
+          <Link to={'/bank/login'} className="bank-header-link-content">
+            로그인
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
